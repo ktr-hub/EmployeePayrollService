250106616a3c648784104abdaf4f0540a40dfc36
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-11-02 09:45:00 +0530

    [Tirupathi] Add. Ability to access Data from Database UC1

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -7,8 +7,9 @@ namespace EmployeePayrollService
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee payroll service!");
-            
-
+            EmployeeRepo repo = new EmployeeRepo();
+            EmployeePayroll employee = new EmployeePayroll();
+            repo.GetAllEmployee();
         }
     }
 }
