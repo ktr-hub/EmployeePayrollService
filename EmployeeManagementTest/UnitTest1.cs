59791c98d406022d1e75cee049d6a1bad8821275
KUMMARI TIRUPATHI RAO <35338095+ktr-hub@users.noreply.github.com>
2020-11-04 22:19:30 +0530

    [Tirupathi] Add. Ability to retrive salary details from database in UC4

--- EmployeeManagementTest/UnitTest1.cs
+++ EmployeeManagementTest/UnitTest1.cs
@@ -21,6 +21,25 @@ namespace EmployeeManagementTest
 
             Assert.AreEqual(10060, empsalary);
         }
-        
+
+        [TestMethod]
+        public void GivenStringName_AbleToRetrieveSalaryDetails()
+        {
+            Salary salary = new Salary();
+            SalaryDetailModel expected = new SalaryDetailModel()
+            {
+                EmployeeId = 2,
+                EmployeeSalary = 1300,
+                EmployeeName = "Vijay",
+                Deductions = 500,
+                TaxablePay = 0,
+                NetPay = 18500,
+                tax = 500
+            };
+
+            SalaryDetailModel actual = salary.getEmployeeObject("Vijay");
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
     }
 }
--- EmployeePayrollService/Salary.cs
+++ EmployeePayrollService/Salary.cs
@@ -54,7 +54,50 @@ namespace EmployeePayrollService
             return 0;
         }
 
-        
+
+        public SalaryDetailModel getEmployeeObject(string empName)
+        {
+            SalaryDetailModel salaryDetail = new SalaryDetailModel();
+            SqlConnection SalaryConnection = ConnectionSetup();
+            try
+            {
+                using (SalaryConnection)
+                {
+                    SalaryConnection.Open();
+                    SqlCommand command = new SqlCommand("sqRetrieveEmployeeSalary", SalaryConnection);
+                    SqlDataReader dr = command.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            String Employeename = dr["empName"].ToString();
+                            if (Employeename.Equals(empName))
+                            {
+                                salaryDetail.EmployeeName = empName;
+                                salaryDetail.EmployeeId = (int)dr["empId"];
+                                salaryDetail.EmployeeSalary = Convert.ToDouble(dr["basicPay"]);
+                                salaryDetail.Deductions = Convert.ToDouble(dr["deductions"]);
+                                salaryDetail.TaxablePay = Convert.ToDouble(dr["taxablePay"]);
+                                salaryDetail.NetPay = Convert.ToDouble(dr["netPay"]);
+                                salaryDetail.tax = Convert.ToDouble(dr["tax"]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(exception.Message);
+            }
+            finally
+            {
+                SalaryConnection.Close();
+            }
+            return salaryDetail;
+        }
+
+
+
     }
 }
 
